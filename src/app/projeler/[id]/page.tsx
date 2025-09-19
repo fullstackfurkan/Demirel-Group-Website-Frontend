9a71446cbb37615ecb056b2f2f599360f39505b8
Furkan Yılmaz <furkanyilmaz870@gmail.com>
2025-09-19 16:27:25 +0300

    Created dynamic projects page

--- src/app/projeler/[id]/page.tsx
+++ src/app/projeler/[id]/page.tsx
@@ -14,9 +14,40 @@ type Project = {
     numberOfApartment: number;
     numberOfStore: number;
     contactNumber: number;
+    detais: string;
+    photos: string[];
 }
 
 export default function ProjectDetailPage() {
+
+    const { id } = useParams();
+    const [project, setProject] = useState<Project | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        if(!id) return;
+
+        const fetchProject = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch(`/api/projects/${id}`);
+                const data = await res.json();
+                setProject(data);
+            }
+            catch(error) {
+                console.error("Proje detayı çekilemedi:", error)
+            }
+            finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProject();
+    }, [id]);
+
+    if(loading) return <p className="p-6">Yükleniyor...</p>
+    if(!project) return <p className="p-6">Proje bulunamadı.</p>
+
     return (
         <div>
             
